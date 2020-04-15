ad7dcac31c931340006ad87e24dec839ec762d65
Adrian S. Almenningen <adrian.s.almenningen@gmail.com>
2020-04-15 21:08:20 +0200

    write superblock

--- fs/dwarfs/dwarfs.h
+++ fs/dwarfs/dwarfs.h
@@ -3,8 +3,74 @@
 
 #include <linux/fs.h>
 #include <linux/types.h>
+#include <linux/uidgid.h>
 
-static const unsigned long DWARFS_MAGIC = 0x1DEADFAD;
+static const int DWARFS_BLOCK_SIZE = 512; /* Size per block in bytes. TODO: experiment with different sizes */
+static const unsigned long DWARFS_MAGIC = 0xDECAFBAD; /* Because caffeine is the only reason this is progressing at all */
+static const unsigned long DWARFS_SUPERBLOCK_BLOCKNUM = 1; /* Default to 1, does this have to be dynamic??? */
+
+typedef unsigned long uint64_t;
+typedef unsigned int uint32_t;
+typedef unsigned short
+
+struct dwarfs_superblock {
+    __le64 dwarfs_magic; /* Magic number */
+
+    /* FS structure and storage information */
+    __le64 dwarfs_blockc; /* number of blocks */
+    __le64 dwarfs_reserved_blocks; /* number of reserved blocks */
+    __le64 dwarfs_free_blocks_count; /* Number of free blocks in the volume */
+    __le64 dwarfs_free_inodes_count; /* Number of free inodes in the volume (let's aim to never let this be zero) */
+    __le64 dwarfs_data_start_block; /* Block at which data storage starts */
+    __le64 dwarfs_block_size; /* Size of disk blocks */
+    __le64 dwarfs_root_inode; /* root inode */
+    __le64 dwarfs_inodec; /* Number of inodes */
+    __le64 dwarfs_blocks_per_group; /* Number of blocks in a disk group */
+    __le64 dwarfs_frags_per_group; /* Number of fragments in a disk group */
+    __le64 dwarfs_inodes_per_group; /* Number of inodes in a disk group */
+
+    /* Time data */
+    __le64 dwarfs_wtime; /* Time of last write */
+    __le64 dwarfs_mtime; /* Time of last mount */
+
+    /* General FS & volume information */
+    __le16 dwarfs_def_resuid; /* Default user ID for reserved blocks */
+    __le16 dwarfs_def_resgid; /* Default group ID for reserved blocks */
+    __le64 dwarfs_version_num; /* Versions might not matter ... backwards/forwards compatibility???? */
+    __le64 dwarfs_os; /* Which OS created the fs (Can probably be deleted, no one outside Linux would use this) */
+
+    /* Add padding to fill the block? */
+};
+
+struct dwarfs_superblock_info {
+    uint64_t dwarfs_fragsize; /* Size of fragmentations in bytes */
+    uint64_t dwarfs_inodes_per_block; /* inodes per block */
+    uint64_t dwarfs_frags_per_group; /* Fragmentations per group */
+    uint64_t dwarfs_blocks_per_group; /* Blocks per group */
+    uint64_t dwarfs_inodes_per_group; /* iNodes per FS group */
+    uint64_t dwarfs_inode_tableblocks_per_group; /* iNode table blocks per FS group */
+    uint64_t dwarfs_grpdescriptor_count; /* Total number of group descriptors */
+    uint64_t dwarfs_descriptor_per_block; /* Group descriptors per block */
+    uint64_t dwarfs_groupc; /* Number of groups in FS */
+    uint64_t dwarfs_sb_blocknum; /* block number of the superblock */
+
+    struct buffer_head *dwarfs_bufferhead; /* Buffer Head containing the superblock */
+    struct dwarfs_superblock *dfsb; /* Super block in the buffer */
+    struct buffer_head **dwarfs_group_desc; /* Group descriptors */
+    struct dax_device *dwarfs_dax_device; /* Direct Access Device */
+
+    uint32_t dwarfs_inodesize; /* Size of inodes */
+    uint64_t dwarfs_first_inum; /* First inode number (ID) */
+    uint64_t dwarfs_dir_count; /* Number of directories */
+    kgid_t dwarfs_resgid; /* GID of reserved blocks */
+    kuid_t dwarfs_resuid; /* UID of reserved blocks */
+
+};
+
+static inline struct dwarfs_superblock_info *DWARFS_SB(struct super_block *sb)
+{
+	return (dwarfs_superblock_info *)sb->s_fs_info;
+}
 
 /* cache.c */
 //extern void fat_cache_inval_inode(struct inode *inode);
--- fs/dwarfs/super.c
+++ fs/dwarfs/super.c
@@ -1,9 +1,13 @@
 #include <linux/fs.h>
+#include <linux/buffer_head.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/time.h>
 #include <linux/ktime.h>
+#include <linux/dax.h>
+#include <linux/uidgid.h>
+#include <linux/limits.h>
 
 #include "dwarfs.h"
 
@@ -14,23 +18,170 @@ MODULE_DESCRIPTION("DwarFS filesystem for bachelor project Computer Science @ VU
 
 /* Mounts the filesystem and returns the DEntry of the root directory */
 static struct dentry *dwarfs_mount(struct file_system_type *type, int flags, char const *dev, void *data) {
-    struct dentry *const entry = mount_bdev(type, flags, dev, data, dwarfs_generate_sb);
+    struct dentry *const entry = mount_bdev(type, flags, dev, data, dwarfs_fill_super);
 
     if(IS_ERR(entry)) pr_err("Failed to mount DwarFS\n");
     else pr_debug("DwarFS mounted successfully\n");
     return entry; // root DEntry
 }
 
+/* Read the superblock */
+static struct dwarfs_superblock *dwarfs_read_superblock(struct super_block *sb) {
+    struct dwarfs_superblock *dwarfsb = kzalloc(sizeof(struct dwarfs_superblock), GFP_NOFS); // Allocate memory for sb, GFP_NOFS blocks FS activity while allocating
+    struct buffer_head *bh;
+    struct dwarfs_disk_superblock *ddsb;
+
+    if(!dwarfsb) {
+        pr_err("DwarFS failed to allocate superblock\n");
+        return NULL;
+    }
+    bh = sb_bread(sb, 0);
+    if(!bh) {
+        pr_err("Couldn't read the superblock\n");
+        kfree(dwarfsb);
+        return NULL;
+    }
+    ddsb = bh->b_data;
+    // Write to dwarfsb
+    brelse(bh);
+    if(dwarfsb->dwarfs_magic != DWARFS_MAGIC) {
+        pr_err("Dwarfs got wrong magic number: 0x%x, expected: 0x%x\n", dwarfsb->dwarfs_magic, DWARFS_MAGIC);
+        kfree(dwarfsb);
+        return NULL;
+    }
+
+    // DEBUG, remove in final version!!!!!
+    pr_debug("Dwarfs superblock:\n
+                \tmagicnum        = 0x%x\n
+                \tinode blocks    = %lu\n
+                \tinode count     = %lu\n
+                \treserved_blocks = %lu\n
+                \tblocksize       = %lu\n
+                \troot inode      = %lu\n",
+                dwarfsb->dwarfs_magic, dwarfsb->dwarfs_blockc, dwarfsb->dwarfs_inodec,
+                dwarfsb->dwarfs_reserved_blocks, dwarfsb->dwarfs_block_size, dwarfsb->dwarfs_root_inode);
+
+    return dwarfsb;
+
+}
+
+
+
+void dwarfs_superblock_sync(struct super_block *sb, struct dwarfs_superblock *dfsb, int wait) {
+    mark_buffer_dirty(DWARFS_SB(sb)->dwarfs_bufferhead);
+    if(wait)
+        sync_dirty_buffer(DWARFS_SB(sb)->dwarfs_bufferhead);
+}
+
+static void dwarfs_write_super(struct super_block *sb) {
+    struct dwarfs_superblock *dfsb = DWARFS_SB(sb)->dfsb;
+    dwarfs_superblock_sync(sb, dfsb, 1);
+}
+
 /* Generate the Superblock when mounting the filesystem */
-static int dwarfs_generate_sb(struct super_block *sb, void *data, int silent) {
+static int dwarfs_fill_super(struct super_block *sb, void *data, int silent) {
+
+    struct dax_device *dax;
     struct inode *root = NULL;
     struct timespec64 ts;
+    struct buffer_head *bh;
+    struct dwarfs_superblock *dfsb;
+    struct dwarfs_superblock_info *dfsb_i;
+    uint64_t logical_sb_blocknum;
+    uint64_t offset = 0;
+    int blocksize;
 
-    /* Add the magic number and available super operations */
+    dax = fs_dax_get_by_bdev(sb->s_bdev);
+    dfsb_i = kzalloc(sizeof(dwarfs_superblock_info), GFP_KERNEL);
+    if(!dfsb_i) {
+        pr_err("DwarFS failed to allocate superblock information structure!\n");
+        return -ENOMEM;
+    }
+
+    sb->s_fs_info = dfsb_i;
+    dfsb_i->dwarfs_sb_blocknum = DWARFS_SUPERBLOCK_BLOCKNUM;
+    dfsb_i->dwarfs_dax_device = dax;
+
+    /* 
+     * Making sure that the physical disk's block size isn't
+     * larger than the filesystem's defined block size.
+     * If so, use the disk's blocksize. Otherwise, default.
+     */
+    blocksize = sb_min_blocksize(sb, DWARFS_BLOCK_SIZE);
+    if(!blocksize) {
+        pr_err("Dwarfs failed to set blocksize!\n");
+        return -EINVAL;
+    }
+
+    /* If the blocksize isn't the same as default, calculate offset. */
+    if(blocksize != DWARFS_BLOCK_SIZE) {
+        logical_sb_blocknum = (DWARFS_SUPERBLOCK_BLOCKNUM * DWARFS_BLOCK_SIZE) / blocksize;
+        offset = (DWARFS_SUPERBLOCK_BLOCKNUM * DWARFS_BLOCK_SIZE) % blocksize;
+    } else logical_sb_blocknum = DWARFS_SUPERBLOCK_BLOCKNUM;
+
+    bh = sb_bread(sb, logical_sb_blocknum);
+    if(!bh) {
+        pr_err("Dwarfs failed to read superblock!\n");
+        return -EINVAL;
+    }
+    dfsb = (struct dwarfs_superblock*)(((char*)bh->b_data) + offset);
+    dfsb_i->dfsb = dfsb;
+    sb->s_magic = le64_to_cpu(dfsb->dwarfs_magic);
+
+    if(sb->s_magic != DWARFS_MAGIC) {
+        pr_err("Dwarfs got wrong magic number: 0x%x, expected: 0x%x\n", sb->s_magic, DWARFS_MAGIC);
+        return -EINVAL;
+    }
+    else pr_debug("Dwarfs got correct magicnum: 0x%x\n", sb->s_magic);
+
+    if(sb->s_blocksize != blocksize) {
+        pr_err("Dwarfs blocksize mismatch: %lu vs %lu\n", sb->s_blocksize, blocksize);
+    }
+
+    dfsb_i->dwarfs_resgid = make_kgid(&init_user_ns, le16_to_cpu(dfsb->dwarfs_def_resgid));
+    dfsb_i->dwarfs_resuid = make_kuid(&init_user_ns, le16_to_cpu(dfsb->dwarfs_def_resuid));
+
+    sb->s_maxbytes = 512; /* TODO: Make this a defined const or dynamic */
+    sb->s_max_links = 512; /* TODO: Make defined const or dynamic */
+    sb->s_time_min = S64_MIN;
+    sb->s_time_max = S64_MAX;
+
+    /* Values gotten from EXT2, experiment to fit better for dwarfs. */
+    dfsb_i->dwarfs_inodesize = 128;
+    dfsb_i->dwarfs_first_inum = 11;
+    dfsb_i->dwarfs_inodes_per_block = sb->s_blocksize / dfsb_i->dwarfs_inodesize;
+    if(dfsb_i->dwarfs_inodes_per_block <= 0) {
+        pr_err("Dwarfs: inodes per block = 0!\n");
+        return -EINVAL;
+    }
+
+    dfsb_i->dwarfs_bufferhead = bh;
+    
+    root = dwarfs_iget(sb, 2); // ROOT number 2, taken from EXT2. Make a constant and possibly change for DwarFS!!!!
+    if(IS_ERR(root)) {
+        pr_err("Dwarfs got error code when getting the root node!\n");
+        return PTR_ERR(root);
+    }
+    if(!S_ISDIR(root->i_mode) || !root->i_blocks || !root->i_size) {
+        iput(root);
+        pr_err("Dwarfs: Root node corrupt!\n");
+        return -EINVAL;
+    }
+
+    sb->s_root = d_make_root(root);
+    if(!sb->s_root) {
+        pr_err("Dwarfs: Couldn't get root inode!\n");
+        return -ENOMEM;
+    }
+    dwarfs_write_super(sb);
+    return 0;
+/*
+
+    //Add the magic number and available super operations
     sb->s_magic = DWARFS_MAGIC;
     sb->s_op = &dwarfs_super_operations;
 
-    /* Create the Root inode */
+    // Create the Root inode
     root = new_inode(sb);
     if(!root) {
         pr_err("Failed to allocate root iNode!\n");
@@ -39,19 +190,19 @@ static int dwarfs_generate_sb(struct super_block *sb, void *data, int silent) {
 
     ktime_get_ts64(&ts);
 
-    /* Define inode data. Currently using fictive data, as writing isn't implemented */
+    // Define inode data. Currently using fictive data, as writing isn't implemented
     root->i_ino = 0;
     root->i_sb = sb;
     root->i_atime = root->i_mtime = root->i_ctime = ts;
     inode_init_owner(root, NULL, S_IFDIR);
 
-    /* Make the root DEntry for the superblock */
+    // Make the root DEntry for the superblock
     sb->s_root = d_make_root(root);
     if(!sb->s_root) {
         pr_err("Failed to create Root!\n");
         return -ENOMEM;
     }
-    
+    */
     return 0;
 }
 
@@ -78,10 +229,13 @@ static void __exit dwarfs_exit(void) {
         pr_err("Encountered error code when unregistering DwarFS\n");
 }
 
-// TODO: actually make this useful
-/* Need to figure out what this is really meant to do */
+/* Destroy the superblock when unmounting */
 static void dwarfs_put_super(struct super_block *sb) {
-    pr_debug("Ayy Lmao super block destroyed\n");
+    struct dwarfs_superblock *dwarfsb = DWARFS_SB(sb)->dfsb;
+    if(dwarfsb)
+        kfree(dwarfsb);
+    sb->s_fs_info = NULL;
+    pr_debug("DwarFS superblock destroyed successfully.\n");
 }
 
 static struct super_operations const dwarfs_super_operations = { .put_super = dwarfs_put_super, };
