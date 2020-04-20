630e82057a08e4dab4704e8d84b7557bdb73edb7
Adrian S. Almenningen <adrian.s.almenningen@gmail.com>
2020-04-20 22:07:34 +0200

    Test moving inode func to super.c

--- fs/dwarfs/super.c
+++ fs/dwarfs/super.c
@@ -58,6 +58,86 @@ MODULE_DESCRIPTION("DwarFS filesystem for bachelor project Computer Science @ VU
 */
 
 
+
+struct inode *dwarfs_inode_get(struct super_block *sb, uint64_t ino) {
+    struct inode *inode;
+    struct dwarfs_inode *dinode;
+    struct dwarfs_inode_info *dinode_info;
+    struct buffer_head *bh;
+    int i;
+    uid_t uid;
+    gid_t gid;
+
+    inode = iget_locked(sb, ino);
+    if(!inode)
+        pr_err("Dwarfs: Failed to get inode in iget!\n");
+        return ERR_PTR(-ENOMEM);
+    if(!(inode->i_state & I_NEW)) {// inode already exists, nothing more to do
+        pr_debug("Dwarfs: Found existing inode, returning\n");
+        return inode;
+    }
+
+    pr_debug("Dwarfs: inode at ino %llu does not exist, creating new!\n", ino);
+    
+    /* If it doesn't exist, we need to create it */
+    dinode_info = DWARFS_INODE(inode);
+    dinode = dwarfs_getdinode(inode->i_sb, ino, &bh);
+    if(IS_ERR(dinode)) {
+        pr_err("DwarFS: Got a bad inode of ino: %llu\n", ino);
+        return ERR_PTR(-EFSCORRUPTED);
+    }
+
+    inode->i_mode = le16_to_cpu(dinode->inode_mode);
+    uid = (uid_t)le16_to_cpu(dinode->inode_uid_high);
+    gid = (gid_t)le16_to_cpu(dinode->inode_gid_high);
+
+    i_uid_write(inode, uid);
+    i_gid_write(inode, gid);
+    set_nlink(inode, le64_to_cpu(dinode->inode_linkc));
+
+    inode->i_size = le64_to_cpu(dinode->inode_size);
+    inode->i_atime.tv_sec = (signed)le64_to_cpu(dinode->inode_atime);
+    inode->i_ctime.tv_sec = (signed)le64_to_cpu(dinode->inode_ctime);
+    inode->i_mtime.tv_sec = (signed)le64_to_cpu(dinode->inode_mtime);
+    inode->i_atime.tv_nsec = inode->i_ctime.tv_nsec = inode->i_mtime.tv_nsec = 0;
+    dinode_info->inode_dtime = le32_to_cpu(dinode->inode_dtime);
+    
+    // Now we can check validity.
+    // Among other things, check if the inode is deleted.
+    if(inode->i_nlink == 0 && (inode->i_mode == 0 || dinode_info->inode_dtime)) {
+        pr_err("Dwarfs: inode is stale (deleted!) at ino: %llu\n", ino);
+        return ERR_PTR(-ESTALE);
+    }
+
+    inode->i_blocks = le64_to_cpu(dinode->inode_blocks);
+    dinode_info->inode_flags = le64_to_cpu(dinode->inode_flags);
+    // Set the flags in the inode
+    dinode_info->inode_fragaddr = le64_to_cpu(dinode->inode_fragaddr);
+    dinode_info->inode_fragnum = dinode->inode_fragnum;
+    dinode_info->inode_fragsize = dinode->inode_fragsize;
+    
+    if(i_size_read(inode) < 0) {
+        pr_err("Dwarfs: Couldnt read inode size: ino %llu\n", ino);
+        return ERR_PTR(-EFSCORRUPTED);
+    }
+
+    dinode_info->inode_dtime = 0;
+    dinode_info->inode_state = 0;
+    dinode_info->inode_block_group = (ino - 1) / DWARFS_SB(inode->i_sb)->dwarfs_inodes_per_group;
+    dinode_info->inode_dir_start_lookup = 0;
+
+    for(i = 0; i < DWARFS_NUMBLOCKS; i++)
+        dinode_info->inode_data[i] = dinode->inode_blocks[i];
+    
+    inode->i_op = &dwarfs_dir_inode_operations;
+    inode->i_fop = &dwarfs_dir_operations;
+    inode->i_mapping->a_ops = &dwarfs_aops;
+
+    brelse(bh);
+    unlock_new_inode(inode);
+    return inode;
+}
+
 void dwarfs_superblock_sync(struct super_block *sb, struct dwarfs_superblock *dfsb, int wait) {
     mark_buffer_dirty(DWARFS_SB(sb)->dwarfs_bufferhead);
     if(wait)
